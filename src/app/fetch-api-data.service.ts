dd630e1fa710f7cbb6660c2bf54822579c567e7f
andreawork-hub <andrea.vitova@hotmail.com>
2023-04-18 09:56:52 +0200

    first commit

---
+++ src/app/fetch-api-data.service.ts
@@ -0,0 +1,57 @@
+import { Injectable } from '@angular/core';
+import { catchError } from 'rxjs';
+import {
+  HttpClient,
+  HttpHeaders,
+  HttpErrorResponse,
+} from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { map } from 'rxjs/operators';
+//declaring the api url that will provide data for the client app
+
+const apiUrl = 'https://movie-api-lnmw.onrender.com/';
+
+@Injectable({
+  providedIn: 'root',
+})
+//service will be available everywhere (hence the root)
+export class FetchApiDataService {
+  // Inject the HttpClient module to the constructor params
+  // this will provide HttpClient to the entire class, making it available via this.http
+  constructor(private http: HttpClient) {}
+  // making the api call for the user registration endpoint
+  public userRegistration(userDetails: any): Observable<any> {
+    console.log(userDetails);
+    return this.http
+      .post(apiUrl + 'users', userDetails)
+      .pipe(catchError(this.handleError));
+  }
+
+  getAllMovies(): Observable<any> {
+    const token = localStorage.getItem('token');
+    return this.http
+      .get(apiUrl + 'movies', {
+        headers: new HttpHeaders({
+          Authorization: 'Bearer ' + token,
+        }),
+      })
+      .pipe(map(this.extractResponseData), catchError(this.handleError));
+  }
+
+  // Non-typed response extraction
+  private extractResponseData(res: any): any {
+    const body = res;
+    return body || {};
+  }
+
+  private handleError(error: HttpErrorResponse): any {
+    if (error.error instanceof ErrorEvent) {
+      console.error('Some error occurred:', error.error.message);
+    } else {
+      console.error(
+        `Error Status code ${error.status}, ` + `Error body is: ${error.error}`
+      );
+    }
+    return throwError('Something bad happened; please try again later.');
+  }
+}
